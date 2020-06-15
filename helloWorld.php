d3a46370c9dc3720a1b04d8a7a12dd2527fe875b
Tom <tomhemery@outlook.com>
2020-06-15 12:35:00 +0100

    First commit, added the weird stuff I've been playing with

---
+++ helloWorld.php
@@ -0,0 +1,65 @@
+<html style="background-color:black;">
+    <head>
+        <link rel="stylesheet" type="text/css" href="mainStyle.css">
+        <title>PHP Fun</title>
+        <h1>PHP Fun</h1>
+    </head>
+    <body>
+        <form action ="helloWorld.php" method="post">
+            FruitName<input type="text" name="fruitName"><br>
+            QuantityAvailable<input type="number" name="quantity" value="0"><br>
+            <input type="submit" value = "Add to DB">
+        </form>
+
+        <?php
+            include "./table.php";
+
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbName = "mysuperfundb";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbName);
+
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            if(isset($_POST["fruitName"]) && $_POST["fruitName"] != ""){
+                $fruitName = $conn->real_escape_string(sanitize_string($_POST["fruitName"]));
+                $quantity = $conn->real_escape_string(sanitize_string(isset($_POST["quantity"]) ? $_POST["quantity"] : 0));
+                $sql = "INSERT INTO fruit (fruitID, fruitName, quantityAvailable) VALUES (NULL, ?, ?)";
+                if ($stmt = $conn->prepare($sql)) {
+                    $stmt->bind_param('si', $fruitName, $quantity);
+                    $stmt->execute();
+                }   
+
+                unset($_POST["fruitName"]);
+                unset($_POST["quantity"]);
+            }
+
+            else if(isset($_POST["deleteID"])){
+                $delID = $_POST["deleteID"];
+                $sql = "DELETE FROM fruit WHERE fruitID = ?";
+                if($stmt = $conn->prepare($sql)){
+                    $stmt->bind_param('i', $delID);
+                    $stmt->execute();
+                }
+                unset($_POST["deleteID"]);
+            }
+
+            $table = new AutoTable($conn, "fruit", 0);
+            $table->display();
+
+            function sanitize_string($data){
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                return $data;
+            }
+
+        ?>
+    </body>
+</html>
---
+++ table.php
@@ -0,0 +1,45 @@
+<?php
+    class AutoTable{
+        private $contents = [];
+        private $keyIndex;
+        
+        
+        function __construct(mysqli $conn, $tableName, $keyIndex)
+        {
+            $sql = "SELECT * from " . $tableName;
+            if($stmt = $conn->prepare($sql)){
+                $stmt->execute();
+                $result = $stmt->get_result();
+                if($result){
+                    while($row = $result->fetch_array(MYSQLI_NUM)){
+                        $this->contents[] = $row;
+                    }
+                }
+                $stmt->close();
+                $this->keyIndex = $keyIndex;
+            }
+        }
+
+        function display(){
+            echo "<form action='helloWorld.php' method='post'><table>";
+            echo "<table>";
+            foreach($this->contents as $row){
+                echo "<tr>";
+                foreach($row as $index => $entry){
+                    if($index != $this->keyIndex)
+                        echo "<td>" . $entry . "<td>";
+                }
+                echo "<td><button type='submit' name='deleteID' value = '". $row[$this->keyIndex] ."'>delete</button></td>";
+                $this->echo_random_image();
+                echo "</tr>";
+            }
+            echo "</table>";
+            echo "</form>";
+        }
+
+        private function echo_random_image(){
+            $seed = rand();
+            echo '<td><img src="https://picsum.photos/100/75?nocache=' . $seed . '"/></td>';
+        }
+    }
+?>
